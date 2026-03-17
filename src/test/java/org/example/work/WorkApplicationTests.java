0208c19a32534281e51706f19a70137dd7350156
你是茶米吗？ <2110235571@qq.com>
2026-03-17 14:21:44 +0800

    init

---
+++ src/test/java/org/example/work/WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
